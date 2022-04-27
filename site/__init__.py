52bab2096ac163062d7f8bfecbe8210148085a42
Rafael Cenzano <savagecoder77@gmail.com>
2022-04-26 20:34:01 -0400

    Create initialization file for the flask app

---
+++ site/__init__.py
@@ -0,0 +1,11 @@
+# Flask imports
+from flask import Flask
+
+# Create Flask app
+app = Flask(__name__)
+
+# Add Configurations to app
+app.config.from_pyfile("config.py", silent=True)
+
+# Import all views
+import site.views
