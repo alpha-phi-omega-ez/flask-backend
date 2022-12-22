4cefb42f276f6573b14e25e00823228ea25754ed
Rafael Cenzano <savagecoder77@gmail.com>
2022-12-21 22:13:43 -0800

    remove old code and change to new copied base

--- site/models.py
+++
@@ -1,9 +0,0 @@
-from enum import unique
-from site import db
-
-class User(db.Model):
-    __tablename__ = 'user'
-
-    id = db.Column(db.Integer, primary_key=True, unique=True, nullabel=False)
-    name = db.Column(db.String(255), unique=False, nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
