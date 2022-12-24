33c00e0c9f08aae90c479d711997b5b12b839a12
Rafael Cenzano <savagecoder77@gmail.com>
2022-12-23 19:04:31 -0800

    replace mongo db code with sqlalchemy code

--- flask-server-google/models.py
+++ flask-server-google/models.py
@@ -1,14 +1,46 @@
+from . import db
 
-from mongoengine.document import Document
-from mongoengine.fields import EmailField, ImageField, IntField, StringField, UUIDField
 
-# inheriting from Document class
-class User(Document):
-    meta = {"collection": "User"}
-    uuid=UUIDField()
-    username=StringField(required=True, max_length=100)
-    password=StringField(required=True, max_length=100, default="LOGGEDINWITHGOOGLE")
-    picture=StringField(required=True)
-    email=EmailField()
+# Backtest Subject Codes Table
+class BacktestSubjectCode(db.Model):
+    subject_code = db.Column(db.String(4), primary_key=True, unique=True)
 
-  
+
+# Backtest Classes Table
+class BacktestClasses(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    subject_code = db.Column(db.String(4), nullable=False, unique=False)
+    course_number = db.Column(db.Integer, nullable=False, unique=False)
+    name_of_class = db.Column(db.String(100), nullable=False, unique=False)
+
+
+# Backtest Table
+class Backtest(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    subject_code = db.Column(db.String(4), nullable=False, unique=False)
+    subject_code_cross = db.Column(db.String(4), nullable=True, unique=False)
+    subject_code_cross_extra = db.Column(db.String(4), nullable=True, unique=False)
+    added = db.Column(db.DateTime, nullable=False, unique=False)
+    course_number = db.Column(db.Integer, nullable=False, unique=False)
+    exam = db.Column(db.Boolean, unique=False, default=False)
+    quiz = db.Column(db.Boolean, unique=False, default=False)
+    midterm = db.Column(db.Boolean, unique=False, default=False)
+    year = db.Column(db.Integer, nullable=False, unique=False)
+    semester = db.Column(db.String(1), nullable=False, unique=False)
+
+
+# LAF Locations
+class LAFLocations(db.Model):
+    location = db.Column(db.String(50), primary_key=True, unique=True)
+
+
+# Lost Reports Table
+class LostReport(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name = db.Column(db.String(40), nullable=False, unique=False)
+    last_name = db.Column(db.String(50), nullable=False, unique=False)
+    email = db.Column(db.String(100), nullable=False, unique=False)
+    phone_area_code = db.Column(db.Integer, nullable=False, unique=False)
+    phone_middle = db.Column(db.Integer, nullable=False, unique=False)
+    phone_end = db.Column(db.Integer, nullable=False, unique=False)
+    
