634acb2bd5ee2969884fcb4811ce9b698676d5d6
Rafael Cenzano <savagecoder77@gmail.com>
2022-12-23 19:06:15 -0800

    Modify to use flask_restful and setup for future development

--- flask-server-google/app.py
+++ flask-server-google/app.py
@@ -1,36 +1,24 @@
-import json
-from flask import Flask 
-from flask.wrappers import Response
-from flask.globals import request, session
+from flask import jsonify, redirect, abort, request, session
 import requests
-from dotenv import load_dotenv
-from werkzeug.exceptions import abort
-from werkzeug.utils import redirect
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import os, pathlib
 import google
-from connect_db import connect_db, insert_into_db
 import jwt
 from flask_cors import CORS
-
-app = Flask(__name__)
-load_dotenv()
-CORS(app)
-app.config['Access-Control-Allow-Origin'] = '*'
-app.config["Access-Control-Allow-Headers"]="Content-Type"
+from flask_restful import Resource
+from flask_sqlalchemy import SQLAlchemy
+from . import app, db, api
+from models import Backtest, BacktestClasses, BacktestSubjectCode
 
 # bypass http
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-app.secret_key = os.getenv("SECRET_KEY")
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_ID = app.config["GOOGLE_CLIENT_ID"]
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client-secret.json")
-algorithm = os.getenv("ALGORITHM")
-BACKEND_URL=os.getenv("BACKEND_URL")
-FRONTEND_URL=os.getenv("FRONTEND_URL")
+algorithm = app.config["ALGORITHM"]
+BACKEND_URL = app.config["BACKEND_URL"]
+FRONTEND_URL = app.config["FRONTEND_URL"]
 
-#database connection
-connect_db()
 
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
@@ -39,18 +27,19 @@ flow = Flow.from_client_secrets_file(
         "https://www.googleapis.com/auth/userinfo.email",
         "openid",
     ],
-    redirect_uri=BACKEND_URL+"/callback",
+    redirect_uri=BACKEND_URL + "/callback",
 )
 
 
 # wrapper
 def login_required(function):
     def wrapper(*args, **kwargs):
-        encoded_jwt=request.headers.get("Authorization").split("Bearer ")[1]
-        if encoded_jwt==None:
+        encoded_jwt = request.headers.get("Authorization").split("Bearer ")[1]
+        if encoded_jwt == None:
             return abort(401)
         else:
             return function()
+
     return wrapper
 
 
@@ -59,77 +48,64 @@ def Generate_JWT(payload):
     return encoded_jwt
 
 
-@app.route("/callback")
-def callback():
-    flow.fetch_token(authorization_response=request.url)
-    credentials = flow.credentials
-    request_session = requests.session()
-    token_request = google.auth.transport.requests.Request(session=request_session)
+class Callback(Resource):
+    def post():
+        flow.fetch_token(authorization_response=request.url)
+        credentials = flow.credentials
+        request_session = requests.session()
+        token_request = google.auth.transport.requests.Request(session=request_session)
 
-    id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token, request=token_request,
-        audience=GOOGLE_CLIENT_ID
-    )
-    session["google_id"] = id_info.get("sub")
-    
-    # removing the specific audience, as it is throwing error
-    del id_info['aud']
-    jwt_token=Generate_JWT(id_info)
-    insert_into_db(
-        id_info.get('name'),
-        id_info.get('email'),
-        id_info.get('picture')
-    )
-    return redirect(f"{FRONTEND_URL}?jwt={jwt_token}")
-    """ return Response(
-        response=json.dumps({'JWT':jwt_token}),
-        status=200,
-        mimetype='application/json'
-    ) """
-
-
-@app.route("/auth/google")
-def login():
-    authorization_url, state = flow.authorization_url()
-    # Store the state so the callback can verify the auth server response.
-    session["state"] = state
-    return Response(
-        response=json.dumps({'auth_url':authorization_url}),
-        status=200,
-        mimetype='application/json'
-    )
-
-
-@app.route("/logout")
-def logout():
-    #clear the local storage from frontend
-    session.clear()
-    return Response(
-        response=json.dumps({"message":"Logged out"}),
-        status=202,
-        mimetype='application/json'
-    )
-
-
-@app.route("/home")
-@login_required
-def home_page_user():
-    encoded_jwt=request.headers.get("Authorization").split("Bearer ")[1]
-    try:
-        decoded_jwt=jwt.decode(encoded_jwt, app.secret_key, algorithms=[algorithm,])
-        print(decoded_jwt)
-    except Exception as e: 
-        return Response(
-            response=json.dumps({"message":"Decoding JWT Failed", "exception":e.args}),
-            status=500,
-            mimetype='application/json'
+        id_info = id_token.verify_oauth2_token(
+            id_token=credentials._id_token, request=token_request, audience=GOOGLE_CLIENT_ID
         )
-    return Response(
-        response=json.dumps(decoded_jwt),
-        status=200,
-        mimetype='application/json'
-    )
+        session["google_id"] = id_info.get("sub")
+
+        # removing the specific audience, as it is throwing error
+        del id_info["aud"]
+        jwt_token = Generate_JWT(id_info)
+        #insert_into_db(id_info.get("name"), id_info.get("email"), id_info.get("picture"))
+        #add sql alchemy things
+        return redirect(f"{FRONTEND_URL}?jwt={jwt_token}")
+        """ return Response(
+            response=json.dumps({'JWT':jwt_token}),
+            status=200,
+            mimetype='application/json'
+        ) """
+
+class GoogleAuth(Resource):
+    def get():
+        authorization_url, state = flow.authorization_url()
+        # Store the state so the callback can verify the auth server response.
+        session["state"] = state
+        return jsonify({"auth_url": authorization_url}), 200
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+class LogOut(Resource):
+    def get():
+        # clear the local storage from frontend
+        session.clear()
+        return jsonify({"message": "Logged out"}), 202
+
+
+class Home(Resource):
+    @login_required
+    def get():
+        encoded_jwt = request.headers.get("Authorization").split("Bearer ")[1]
+        try:
+            decoded_jwt = jwt.decode(
+                encoded_jwt,
+                app.secret_key,
+                algorithms=[
+                    algorithm,
+                ],
+            )
+            #print(decoded_jwt)
+        except Exception as e:
+            return jsonify({"message": "Decoding JWT Failed", "exception": e.args}), 500
+        return jsonify(decoded_jwt), 200
+
+api.add_resource(Home, "/home")
+api.add_resource(LogOut, "/logout")
+api.add_resource(GoogleAuth, "/auth/google")
+api.add_resource(Callback, "/callback")
+#api.add_resource(Square, '/square/<int:num>')
