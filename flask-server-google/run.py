92c50c5532afbadfb58ac1868edc6d0517ce5d40
Rafael Cenzano <savagecoder77@gmail.com>
2022-12-23 19:04:10 -0800

    modify run.py for server needs

--- flask-server-google/run.py
+++ flask-server-google/run.py
@@ -1,4 +1,4 @@
-from site import app
+from . import app
 
 if __name__ == "__main__":
-    app.run(ssl_context="adhoc", debug=True, port=8000)
+    app.run(ssl_context="adhoc", port=8000, host="0.0.0.0")
