ef93d030d9fc78b45e806561a2b868730e3aa04a
Rafael Cenzano <savagecoder77@gmail.com>
2022-12-23 19:04:42 -0800

    fix config importing

--- flask-server-google/manage.py
+++ flask-server-google/manage.py
@@ -1,16 +1,17 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from config import TestingConfig, ProductionConfig
 
 from site import app, db
 
-app.config.from_pyfile("config.py")
+app.config.from_object(TestingConfig)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
+manager.add_command("db", MigrateCommand)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
