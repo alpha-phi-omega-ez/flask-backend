927e092efd1f3cc3b4a178bd0100058ccadbbfcc
Rafael Cenzano <savagecoder77@gmail.com>
2023-01-31 23:59:57 -0500

    no production db yet

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class TestingConfig():
     FRONTEND_URL = "http://localhost:3000"
 
 class ProductionConfig(TestingConfig):
-    SQLALCHEMY_DATABASE_URI = ""
+    #SQLALCHEMY_DATABASE_URI = ""
     
     BACKEND_URL = "http://127.0.0.1:8000"
     FRONTEND_URL = "http://localhost:3000"
