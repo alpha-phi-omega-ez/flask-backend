a0122287ec181e5a0e8385f09bf2d4c9d14027dc
Rafael Cenzano <savagecoder77@gmail.com>
2023-02-13 14:23:45 -0500

    remove flask cors

--- apo/__init__.py
+++ apo/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from flask_cors import CORS
-from config import TestingConfig, ProductionConfig
+from config import TestingConfig#, ProductionConfig
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object(TestingConfig)
-CORS(app)
 app.config["Access-Control-Allow-Origin"] = "*"
 app.config["Access-Control-Allow-Headers"] = "Content-Type"
 api = Api(app)
